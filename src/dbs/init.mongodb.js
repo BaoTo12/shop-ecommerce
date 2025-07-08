b2b142e313bbb02471ec250f74f1398d89b8888a
ChiBao <baotochi87@gmail.com>
2025-07-08 16:24:43 +0700

    add config.mongo.js

--- src/dbs/init.mongodb.js
+++ src/dbs/init.mongodb.js
@@ -1,6 +1,7 @@
 "use strict"
+const { db: { host, name, port } } = require("../configs/config.mongo")
 const mongoose = require("mongoose")
-const connectString = "mongodb://localhost:27017/shopDEV"
+const connectString = `mongodb://${host}:${port}/${name}`
 const { countConnect } = require("../helpers/check.connect")
 
 class Database {
