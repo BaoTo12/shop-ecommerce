dfa2c7de1346fad05ff0e8d2abd8d7e4707cf8fb
ChiBao <baotochi87@gmail.com>
2025-07-10 10:35:06 +0700

    fix: fix function findById ApiKeyService to test api

--- src/auth/checkAuth.js
+++ src/auth/checkAuth.js
@@ -11,7 +11,7 @@ const HEADER = {
 const checkApiKey = async (req, res, next) => {
     try {
         // check if api key presents in header
-        const key = req.header[HEADER.API_KEY]
+        const key = req.headers[HEADER.API_KEY]?.toString()
         if (!key) {
             return res.status(403).json({
                 message: "Forbidden Error"
