7a761a6058f164ac3e84e26ebc495f69d4f81c0e
ChiBao <baotochi87@gmail.com>
2025-07-10 11:51:07 +0700

    feat(error-handler): add logic for error handler

--- src/auth/checkAuth.js
+++ src/auth/checkAuth.js
@@ -53,7 +53,14 @@ const checkPermission = (permission) => {
     }
 }
 
+const asyncHandler = fn => {
+    return (req, res, next) => {
+        fn(req, res, next).catch(next)
+    }
+}
+
 module.exports = {
     checkApiKey,
-    checkPermission
+    checkPermission,
+    asyncHandler
 }
