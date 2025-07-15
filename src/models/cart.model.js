cf2d4b5dbbc4a7dec76183cdcaff8e74bba75d6c
ChiBao <baotochi87@gmail.com>
2025-07-15 07:33:48 +0700

    fix(cart): fix error in cart model that is timestamp not timeseries

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -34,7 +34,7 @@ const cartSchema = new Schema({
     }
 }, {
     collection: COLLECTION_NAME,
-    timeseries: {
+    timestamps: {
         createdAt: 'createdOn',
         updatedAt: 'modifiedOn'
     }
