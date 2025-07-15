95285f15f23caabd0275a3ca6e1367e08df9037f
ChiBao <baotochi87@gmail.com>
2025-07-15 08:32:57 +0700

    feat(order): add checkout routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,7 +9,7 @@ router.use(checkApiKey)
 // check Permissions
 router.use(checkPermission("0000"))
 
-
+router.use(`/${process.env.API_VERSION}/api/checkout`, require("./checkout"))
 router.use(`/${process.env.API_VERSION}/api/product`, require("./product"))
 router.use(`/${process.env.API_VERSION}/api/discount`, require("./discount"))
 router.use(`/${process.env.API_VERSION}/api/cart`, require("./cart"))
