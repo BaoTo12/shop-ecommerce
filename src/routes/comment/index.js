05c9a46a133193d895d066d1e4983a352f1c2f69
ChiBao <baotochi87@gmail.com>
2025-07-22 07:22:10 +0700

    feat(comment): add comment routes

---
+++ src/routes/comment/index.js
@@ -0,0 +1,17 @@
+"use strict"
+const express = require("express")
+const { authenticationV2 } = require("../../auth/authUtils");
+const asyncHandler = require("../../helpers/asyncHandler");
+const commentController = require("../../controllers/comment.controller");
+
+
+
+const router = express.Router();
+
+
+router.use(authenticationV2)
+
+router.post("", asyncHandler(commentController.createComment))
+
+
+module.exports = router;
