fb6df1a4ae4765c7ee6c14d25851ec4d2b96334d
ChiBao <baotochi87@gmail.com>
2025-07-22 08:11:07 +0700

    feat(comment): add function to create comment when have parent comment

--- src/services/comment.service.js
+++ src/services/comment.service.js
@@ -1,5 +1,6 @@
 "use strict"
 
+const { BadRequestError, NotFoundError } = require("../core/error.response");
 const commentModel = require("../models/comment.model")
 const { convertToObjectId } = require("../utils/index")
 
@@ -19,13 +20,40 @@ class CommentService {
             comment_parentId: parentCommentId,
             comment_content: content
         })
-
+        let indexValue;
         // Calculate the index for the comment
         // Case:  This is not parent comment
         if (parentCommentId) {
+            // find reply parent
+            const commentParent = await commentModel.findOne({
+                _id: convertToObjectId(parentCommentId)
+            })
+            if (!commentParent) throw new BadRequestError(`There is no comment with ${parentCommentId}::Comment_Parent`)
+            console.log({ commentParent });
+
+
+            indexValue = commentParent.comment_right;
+
+            // update other comments
+            await commentModel.updateMany({
+                comment_productId: convertToObjectId(productId),
+                comment_right: {
+                    $gte: indexValue
+                }
+            }, {
+                $inc: { comment_right: 2 }
+            })
+            await commentModel.updateMany({
+                comment_productId: convertToObjectId(productId),
+                comment_left: {
+                    $gt: indexValue
+                }
+            }, {
+                $inc: { comment_left: 2 }
+            })
 
         } else {
-            let indexValue;
+
             let maxRightValue = await commentModel.findOne({
                 comment_productId: convertToObjectId(productId)
             }, "comment_right", {
@@ -35,11 +63,12 @@ class CommentService {
             })
             // Case: there are other parent comments
             if (maxRightValue) {
-                indexValue = maxRightValue + 1;
+                indexValue = maxRightValue.comment_right + 1;
             } else {
                 indexValue = 1
             }
         }
+        console.log({ indexValue });
 
         comment.comment_left = indexValue;
         comment.comment_right = indexValue + 1;
